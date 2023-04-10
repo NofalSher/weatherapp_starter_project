b72b6625242ddc73fdc53790d8edab2cc834920e
Nofal Sher <84616042+NofalSher@users.noreply.github.com>
2023-04-10 21:57:09 +0500

    Update api_calling.dart

--- lib/api/api_calling.dart
+++ lib/api/api_calling.dart
@@ -1,1 +1,38 @@
+import 'package:http/http.dart';
+import 'dart:convert';
 
+class api_calling {
+  String location;
+  String get_temp;
+  String get_humidity;
+  String get_pressure;
+  String get_air_speed;
+  String get_weather_condition;
+  String main;
+
+  api_calling({this.location}) {}
+
+  void getData() async {
+    var res = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=c72003fb0ae9a2d22008765f590e802e');
+    var response = await get(res);
+    Map data = jsonDecode(response.body);
+    List weather_data = data['weather'];
+    Map weather_main_data = weather_data[0];
+    print(weather_data);
+    print(weather_main_data['main']);
+
+    Map wind = data['wind'];
+    double air_speed = wind['speed'];
+
+    Map temp_data = data['main'];
+
+    double humindity = temp_data['humindity'];
+
+    double temp = temp_data['temp'];
+    print(temp);
+
+    String weather_condition = weather_main_data['description'];
+    String main = weather_main_data['main'];
+  }
+}
