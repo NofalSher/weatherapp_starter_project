edc34799ab0d1099799e204d5befac881f1d6ef0
Nofal Sher <84616042+NofalSher@users.noreply.github.com>
2023-04-11 14:44:18 +0500

    ..

--- lib/Activity/loading.dart
+++ lib/Activity/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp_starter_project/Activity/location.dart';
 import 'package:weatherapp_starter_project/api/api_calling.dart';
 import 'dart:convert';
 
@@ -12,30 +13,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  String? location = 'Islamabad';
   var x;
   void startApp() async {
-    api_Calling instance = api_Calling(location: 'Islamabad');
+    worker instance = worker(location: 'Sydney');
     await instance.getData();
     print(instance.air_speed);
-  }
-
-  Future<void> getData() async {
-    var res = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=c72003fb0ae9a2d22008765f590e802e');
-    var response = await get(res);
-    Map data = jsonDecode(response.body);
-    List get_weather_data = data['weather'];
-    Map get_weather_main_data = get_weather_data[0];
-    print(get_weather_data);
-    print('_________________________________________');
-    print(get_weather_main_data);
+    print(instance.description);
+    print(instance.humidity);
+    print(instance.main);
+    print(instance.temp);
+    x = await instance.air_speed;
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    startApp();
   }
 
   Widget build(BuildContext context) {
--- lib/api/api_calling.dart
+++ lib/api/api_calling.dart
@@ -3,22 +3,17 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 
-class api_Calling {
+class worker {
   String location;
-
-  api_Calling({
-    required this.location,
-  }) {
+  worker({required this.location}) {
     location = this.location;
   }
-  String? get_weather_condition;
-  String? get_Main;
 
   String? temp;
   String? humidity;
   String? air_speed;
 
-  String? weather_condition;
+  String? description;
   String? main;
 
   Future<void> getData() async {
@@ -26,28 +21,25 @@ class api_Calling {
         'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=c72003fb0ae9a2d22008765f590e802e');
     var response = await get(res);
     Map data = jsonDecode(response.body);
+
     List get_weather_data = data['weather'];
     Map get_weather_main_data = get_weather_data[0];
-    print(get_weather_data);
-    print(get_weather_main_data['main']);
+
+    String get_main_desc = get_weather_main_data['main'];
+    String get_desc = get_weather_main_data['description'];
+
+    Map get_temp_data = data['main'];
+    double get_temp = get_temp_data['temp'];
 
     Map get_wind = data['wind'];
     double get_air_speed = get_wind['speed'];
 
-    Map temp_data = data['main'];
-
-    double get_humindity = temp_data['humindity'];
-
-    double get_temp = temp_data['temp'];
-
-    get_weather_condition = get_weather_main_data['description'];
-    get_Main = get_weather_main_data['main'];
+    double get_humidity = get_temp_data['humidity'];
 
     temp = get_temp.toString();
-    humidity = get_humindity.toString();
+    humidity = get_humidity.toString();
     air_speed = get_air_speed.toString();
-
-    weather_condition = get_weather_condition.toString();
-    main = get_Main.toString();
+    description = get_desc;
+    main = get_main_desc;
   }
 }
